296738d26f6b6e7ac0c01bae5275b096a26c1f15
ivancostilla <ivan20punto88@gmail.com>
2021-01-27 21:57:26 -0300

    classname

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -2,12 +2,12 @@ import "./style.css";
 
 const NavBar = ()=> {
 return (
-    <nav className={'navbar'}>
-        <ul className={'ul'}>
-            <li><a className={'a'} href="#">Tienda App</a></li>
-            <li><a className={'a'} href="#">Contact Us</a></li>
-            <li><a className={'a'} href="#">Services</a></li>
-            <li><a className={'a'} href="#">About Us</a></li>
+    <nav className='navbar'>
+        <ul className='ul'>
+            <li><a className='a' href="#">Tienda App</a></li>
+            <li><a className='a' href="#">Contact Us</a></li>
+            <li><a className='a' href="#">Services</a></li>
+            <li><a className='a' href="#">About Us</a></li>
         </ul>
     </nav>
 );

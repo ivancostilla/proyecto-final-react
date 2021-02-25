c72e9f71f1c286744fd40dcf57302aced8b51e15
ivancostilla <ivan20punto88@gmail.com>
2021-02-24 23:49:53 -0300

    clase firebase

---
+++ src/firebase/index.jsx
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+import '@firestore/firebase';
+
+  const app = firebase.initializeApp = ({
+    apiKey: "AIzaSyBoSXvxpOvy1zF8TTPVG31xXqB9tGuq9y0",
+    authDomain: "reactcoderhouse-12b45.firebaseapp.com",
+    projectId: "reactcoderhouse-12b45",
+    storageBucket: "reactcoderhouse-12b45.appspot.com",
+    messagingSenderId: "167123040137",
+    appId: "1:167123040137:web:a1dfb1bb055701cc749358",
+    measurementId: "G-WJQ6T43Y9V"
+  });
+  firebase.analytics();
